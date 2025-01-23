ac963b5162f3dfde5e0fde2220e72cdeb1424a56
kaushalsisat@gmail.com <kaushalsisat@gmail.com>
2025-01-24 01:55:29 +0530

    by ks

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build", // This makes sure Vite outputs to the 'build' folder
+  },
+  base: "/Countdown-Timer/", // Add this line
+
 })
